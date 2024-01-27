88b9df30003b9d65af4ff3db6965ae1d6ef69602
Kirill Kravchuk <k.a.kravchuk@gmail.com>
2024-01-27 15:01:46 -0500

    works

--- main.cpp
+++ main.cpp
@@ -4,36 +4,47 @@
 int main(int argc, char* argv[]) {
 
 	Matrix inputs(10, 3);
+	Matrix labels(10, 1);
 
 	inputs[0][0] = 1;
 	inputs[0][1] = 1;
+	labels[0][0] = 1;
 
 	inputs[1][0] = 2;
 	inputs[1][1] = 1;
+	labels[1][0] = 1;
 
 	inputs[2][0] = 2;
 	inputs[2][1] = 3;
+	labels[2][0] = 1;
 
 	inputs[3][0] = 3;
 	inputs[3][1] = 2;
+	labels[3][0] = 1;
 
 	inputs[4][0] = 4;
 	inputs[4][1] = 4;
+	labels[4][0] = 1;
 
 	inputs[5][0] = -1;
 	inputs[5][1] = -1;
+	labels[5][0] = -1;
 
 	inputs[6][0] = -1;
 	inputs[6][1] = -3;
+	labels[6][0] = -1;
 
 	inputs[7][0] = -2;
 	inputs[7][1] = -2;
+	labels[7][0] = -1;
 
 	inputs[8][0] = -3;
 	inputs[8][1] = -1;
+	labels[8][0] = -1;
 
 	inputs[9][0] = -4;
 	inputs[9][1] = -4;
+	labels[9][0] = -1;
 
 	for (int i = 0; i < 10; i++) {
 		inputs[i][2] = 1;
@@ -42,9 +53,52 @@ int main(int argc, char* argv[]) {
 	
 	inputs.print();
 
-	Perceptron perc(3);
+	Perceptron perc;
 
-	perc.fit(inputs, 0.5, 100);
-	perc.plot2D(inputs);
+	perc.fit(inputs, labels, 0.5, 10);
+	perc.plot2D(inputs, labels, "perc1.png");
+
+	inputs[0][0] = -1;
+	inputs[0][1] = 1;
+	labels[0][0] = 1;
+
+	inputs[1][0] = -2;
+	inputs[1][1] = 1;
+	labels[1][0] = 1;
+
+	inputs[2][0] = -2;
+	inputs[2][1] = 3;
+	labels[2][0] = 1;
+
+	inputs[3][0] = -3;
+	inputs[3][1] = 2;
+	labels[3][0] = 1;
+
+	inputs[4][0] = -4;
+	inputs[4][1] = 4;
+	labels[4][0] = 1;
+
+	inputs[5][0] = 1;
+	inputs[5][1] = -1;
+	labels[5][0] = -1;
+
+	inputs[6][0] = 1;
+	inputs[6][1] = -3;
+	labels[6][0] = -1;
+
+	inputs[7][0] = 2;
+	inputs[7][1] = -2;
+	labels[7][0] = -1;
+
+	inputs[8][0] = 3;
+	inputs[8][1] = -1;
+	labels[8][0] = -1;
+
+	inputs[9][0] = 4;
+	inputs[9][1] = -4;
+	labels[9][0] = -1;
+
+	perc.fit(inputs, labels, 0.5, 10);
+	perc.plot2D(inputs, labels, "perc2.png");
 
 }
--- perceptron.h
+++ perceptron.h
@@ -7,6 +7,16 @@
 #include <stdint.h>
 #include "supportLib.hpp"
 #include "pbPlots.hpp"
+#include <string>
+
+enum {
+	COL_IDX_X = 0,
+	COL_IDX_Y,
+	COL_IDX_BIAS,
+	NUM_COLS
+};
+
+
 struct Matrix {
 	Matrix(int num_rows, int num_cols) {
 		m_num_rows = num_rows;
@@ -52,20 +62,19 @@ struct Matrix {
 class Perceptron
 {
 public:
-	Perceptron(int num_features) {
-		m_num_features = num_features;
-		weights = new double[num_features];
+	Perceptron() {
+		weights = new double[NUM_COLS];
 		std::default_random_engine generator;
 		std::normal_distribution<double> dist(1, 0.1);
 		std::cout << "Constructing perceptron. Initial weights: \n";
-		for (int i = 0; i < num_features; i++) {
+		for (int i = 0; i < NUM_COLS; i++) {
 			double number = dist(generator);
-			weights[i] = number / (double)std::sqrt(num_features);
+			weights[i] = number / (double)std::sqrt((double)NUM_COLS);
 			std::cout << weights[i] << "\n";
 		} 
 	}
 
-	int fit(Matrix X, double learning_rate, int epochs) {
+	int fit(Matrix X, Matrix labels, double learning_rate, int epochs) {
 		double dot_res = 0;
 		int d = 0;
 		int i, j;
@@ -75,17 +84,18 @@ public:
 			for (i = 0; i < X.get_num_rows(); i++) {
 				// dot product weights and input row
 				std::cout << "calculating dot product...\n";
-				for (j = 0; j < m_num_features; j++) {
-					std::cout << "X[i][j] = " << X[i][j] << "; weights[j] = " << weights[j] << "\n";
-					dot_res += X[i][j] * weights[j];
-				}
+
+				dot_res = X[i][COL_IDX_X] * weights[COL_IDX_X] + 
+						  X[i][COL_IDX_Y] * weights[COL_IDX_Y] + 
+					      X[i][COL_IDX_BIAS] * weights[COL_IDX_BIAS];
+
 				std::cout << "dot product = " << dot_res << "\n";
 				// if d is > 0 no weight update needed
 				d = step(dot_res);
 				std::cout << "d = " << d << "\n";
 				// update weights
-				for (j = 0; j < m_num_features; j++) {
-					weights[j] += -learning_rate * d * X[i][j];
+				for (j = 0; j < NUM_COLS; j++) {
+					weights[j] += learning_rate * ((double)labels[i][0] - d)* X[i][j];
 				}
 				dot_res = 0;
 			}
@@ -100,16 +110,25 @@ public:
 		return ret;
 	}
 
-	void plot2D(Matrix X) {
+	void plot2D(Matrix X, Matrix labels, std::string name) {
 		RGBABitmapImageReference *imageReference = CreateRGBABitmapImageReference();
-		std::vector<double> xdots, ydots;
+		std::vector<double> xdots, ydots, xdots_1, ydots_1, xdots_2, ydots_2;
 		std::vector<double> x_line, y_line, x_space, y_space;
 		for (int i = 0; i < X.get_num_rows(); i++) {
-			xdots.push_back(X[i][0]);
-			ydots.push_back(X[i][1]);
-		}
+			xdots.push_back(X[i][COL_IDX_X]);
+			ydots.push_back(X[i][COL_IDX_Y]);
+			if (*labels[i] > 0) {
+				xdots_1.push_back(X[i][COL_IDX_X]);
+				ydots_1.push_back(X[i][COL_IDX_Y]);
+				std::cout << "class 1 point: " << X[i][COL_IDX_X] << "\n";
 
-		double a = 0.1;
+			}
+			else {
+				xdots_2.push_back(X[i][COL_IDX_X]);
+				ydots_2.push_back(X[i][COL_IDX_Y]);
+				std::cout << "class 2 point: " << X[i][COL_IDX_X] << "\n";
+			}
+		}
 
 		double min_x = (double)*std::min_element(xdots.begin(), xdots.end());
 		double max_x = (double)*std::max_element(xdots.begin(), xdots.end());
@@ -123,22 +142,18 @@ public:
 		StringReference* errorMessage = CreateStringReferenceLengthValue(0, L' ');
 		std::cout << "drawing plot\n";
 
-
-		ScatterPlotSeries* series = GetDefaultScatterPlotSeriesSettings();
-		series->xs = &xdots;
-		series->ys = &ydots;
-		series->linearInterpolation = false;
-		series->pointType = toVector(L"dots");
-		series->color = CreateRGBColor(1, 0, 0);
-
-		ScatterPlotSeries* series2 = GetDefaultScatterPlotSeriesSettings();
-		series2->xs = &x_line;
-		series2->ys = &y_line;
-		series2->linearInterpolation = true;
-		series2->lineType = toVector(L"solid");
-		series2->lineThickness = 2;
-		series2->color = CreateRGBColor(0, 0, 1);
-
+		for (auto coord : xdots_1) {
+			std::cout << "xdots_1: " << coord << "\n";
+		}
+		for (auto coord : xdots_2) {
+			std::cout << "xdots_2: " << coord << "\n";
+		}
+		for (auto coord : ydots_1) {
+			std::cout << "ydots_1: " << coord << "\n";
+		}
+		for (auto coord : ydots_2) {
+			std::cout << "ydots_2: " << coord << "\n";
+		}
 		ScatterPlotSettings* settings = GetDefaultScatterPlotSettings();
 		settings->width = 600;
 		settings->height = 400;
@@ -147,20 +162,44 @@ public:
 		settings->title = toVector(L"");
 		settings->xLabel = toVector(L"");
 		settings->yLabel = toVector(L"");
+
+		ScatterPlotSeries* series = GetDefaultScatterPlotSeriesSettings();
+		series->xs = &xdots_1;
+		series->ys = &ydots_1;
+		series->linearInterpolation = false;
+		series->pointType = toVector(L"dots");
+		series->color = CreateRGBColor(1, 0, 0);
 		settings->scatterPlotSeries->push_back(series);
+
+		ScatterPlotSeries* series2 = GetDefaultScatterPlotSeriesSettings();
+		series2->xs = &xdots_2;
+		series2->ys = &ydots_2;
+		series2->linearInterpolation = false;
+		series2->pointType = toVector(L"dots");
+		series2->color = CreateRGBColor(0, 1, 0);
 		settings->scatterPlotSeries->push_back(series2);
+
+		ScatterPlotSeries* series3 = GetDefaultScatterPlotSeriesSettings();
+		series3->xs = &x_line;
+		series3->ys = &y_line;
+		series3->linearInterpolation = true;
+		series3->lineType = toVector(L"solid");
+		series3->lineThickness = 2;
+		series3->color = CreateRGBColor(0, 0, 1);
+
+		settings->scatterPlotSeries->push_back(series3);
 
 		bool success = DrawScatterPlotFromSettings(imageReference, settings, errorMessage);
 
 		if (success) {
 			std::vector<double>* pngdata = ConvertToPNG(imageReference->image);
-			WriteToFile(pngdata, "example3.png");
+			WriteToFile(pngdata, name);
 			DeleteImage(imageReference->image);
 		}
 		else {
 			std::cerr << "Error: ";
-			for (wchar_t c : *errorMessage->string) {
-				std::cerr << c;
+			for (char c : *errorMessage->string) {
+				std::cout << c;
 			}
 			std::cerr << std::endl;
 		}
@@ -169,5 +208,4 @@ public:
 	}
 private:
 	double* weights;
-	int m_num_features;
 };
